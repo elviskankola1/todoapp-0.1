7591fa2dcf1372968a4fadd0198ddba62339155a
elviskankola1 <elviskankola1@gmail.com>
2019-03-15 22:08:34 +0200

    AFfichage des task dans la vue welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,9 +12,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <a href="">CREER UNE TASK</a>
 <h1>LISTE TACHE <small>nb</small></h1><hr>
-	<p>Nom tache et date</p>
-
-
+<?php if ($task):?>
+<?php foreach ($task as $value):?>
+	<p><?php echo $value->description; ?></p>
 	
+<?php endforeach; ?>
+<?php else:?>
+	<h2>Aucune Tache</h2>
+<?php endif; ?>
 </body>
 </html>
