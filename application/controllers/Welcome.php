33ee41f5e5338f5455ad53a803dd8de9119ebb16
elviskankola1 <elviskankola1@gmail.com>
2019-03-15 21:39:01 +0200

    [NEW] ajout d'une methode d'ajout d'un task dans ce controller(welcome.php)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,4 +8,18 @@ class Welcome extends CI_Controller
     {
         $this->load->view('welcome_message');
     }
+
+    public function addTask()
+    {
+        $this->form_validation->set_rules('description', 'description', 'trim|required|min_length[5]|max_length[12]');
+        $this->form_validation->set_rules('datedebut', 'date de debut', 'trim|required');
+        $this->form_validation->set_rules('datefin', 'date de la fin', 'trim|required');
+        if ($this->form_validation->run()) {
+            $todoDescription = strip_tags($this->input->post('description'));
+            $todoDateDebut = $this->input->post('datedebut');
+            $todoDateFin = $this->input->post('datefin');
+        } else {
+            $this->load->view('');
+        }
+    }
 }
