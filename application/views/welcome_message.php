753ba736cda1e6d462be8c1204daf0f0c3c3f772
elviskankola1 <elviskankola1@gmail.com>
2019-03-15 21:26:18 +0200

    [NEW]creation d'un formulaire pour creer une nouvelle TASK

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -16,7 +16,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 	<form>
-	
+		<input type="text" name="description">
+		<input type="date" name="datedebut">
+		<input type="date" name="datefin">
+		<input type="submit" value="SEND">
 	
 	</form>
 </body>
