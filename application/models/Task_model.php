7aa379c5d25c55e2db1fd3cabfd7808a8795e026
elviskankola1 <elviskankola1@gmail.com>
2019-03-15 21:53:21 +0200

    [NEW] implementation d'une method d'ajout d'un task dans la table tache de la DB

--- application/models/Task_model.php
+++ application/models/Task_model.php
@@ -2,6 +2,23 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Model
+class Task_model extends CI_Model
 {
+    private $todoTableTask = 'tache';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function addTaskModel($todoDescription, $todoDateDebut, $todoDateFin)
+    {
+        $data = [
+            'description' => $todoDescription,
+            'date_debut' => $todoDateDebut,
+            'date_fin' => $todoDateFin,
+        ];
+
+        $this->db->insert($this->todoTableTask, $data);
+    }
 }
