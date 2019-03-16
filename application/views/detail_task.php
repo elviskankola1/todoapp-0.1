7aa9c64281a05c3440056a4e513395ef5975fe6d
elviskankola1 <elviskankola1@gmail.com>
2019-03-16 11:19:23 +0200

    final

--- application/views/detail_task.php
+++ application/views/detail_task.php
@@ -2,7 +2,7 @@
 <?php foreach ($taskForId as $value):?>
 <h2>
     <form>
-        <input type="text" value="<?php echo $value->description; ?>" name="desc"><br>
+        <input type="text" value="<?php echo $value->description; ?>" name="description"><br>
         <input type="date" value="<?php echo $value->date_debut; ?>" name="debut"><br>
         <input type="date" value="<?php echo $value->date_fin; ?>" name="fin"><br>
         <input type="hidden" value="<?php echo $value->id; ?>" name="id"><br>
