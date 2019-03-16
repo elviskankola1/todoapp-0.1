4da26b257524ef5487a5a8611a1675e9a1fe49ab
elviskankola1 <elviskankola1@gmail.com>
2019-03-16 11:47:26 +0200

    fini

--- application/views/detail_task.php
+++ application/views/detail_task.php
@@ -1,7 +1,7 @@
 
 <?php foreach ($taskForId as $value):?>
 <h2>
-    <form action="<?php echo site_url('welcome/updateOneTask'); ?>">
+    <form action="<?php echo site_url('welcome/updateOneTask'); ?>" method="POST">
         <input type="text" value="<?php echo $value->description; ?>" name="description"><br>
         <input type="date" value="<?php echo $value->date_debut; ?>" name="debut"><br>
         <input type="date" value="<?php echo $value->date_fin; ?>" name="fin"><br>
