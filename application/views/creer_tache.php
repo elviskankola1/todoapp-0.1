cba2d735afd3c112b682caa069caeb060cab7e9a
elviskankola1 <elviskankola1@gmail.com>
2019-03-16 08:46:13 +0200

    [UPDATE] ajoute de l'attriut action dans le formulaire

--- application/views/creer_tache.php
+++ application/views/creer_tache.php
@@ -1,4 +1,4 @@
-<form>
+<form action="<?php echo site_url('welcome/addTask'); ?>" method="POST">
 		<input type="text" name="description">
 		<input type="date" name="datedebut">
 		<input type="date" name="datefin">
