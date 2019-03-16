6275f1ccc95e70e2138e81c318a07e14dda22c88
elviskankola1 <elviskankola1@gmail.com>
2019-03-16 10:52:08 +0200

    [UPDATE] formulaire fini pour la modification

--- application/views/detail_task.php
+++ application/views/detail_task.php
@@ -1,9 +1,12 @@
 
 <?php foreach ($taskForId as $value):?>
 <h2>
-    
-    <textarea col="7" rows="8" value="">
-    
-    </textarea>
+    <form>
+        <input type="text" value="<?php echo $value->description; ?>" name="desc"><br>
+        <input type="date" value="<?php echo $value->date_debut; ?>" name="debut"><br>
+        <input type="date" value="<?php echo $value->date_fin; ?>" name="fin"><br>
+        <input type="hidden" value="<?php echo $value->id; ?>" name="id"><br>
+        <input type="submit" value="UPDATE">
+    </form>
 </h2>
 <?php endforeach; ?>
