8057f70ee5adae3d5d75e6a640bfae796ce7bd5d
elviskankola1 <elviskankola1@gmail.com>
2019-03-17 07:25:45 +0200

    [NEW] utilisation de l'helper form sur le formulaire

--- application/views/creer_tache.php
+++ application/views/creer_tache.php
@@ -1,6 +1,9 @@
 <form action="<?php echo site_url('welcome/addTask'); ?>" method="POST">
-		<input type="text" name="description">
-		<input type="date" name="datedebut">
-		<input type="date" name="datefin">
+		<input type="text" name="description" value="<?php echo set_value('description'); ?>"><br>
+		<?php echo form_error('description'); ?>
+		<input type="date" name="datedebut" value="<?php echo set_value('datedebut'); ?>"><br>
+		<?php echo form_error('datedebut'); ?>
+		<input type="date" name="datefin" value="<?php echo set_value('datefin'); ?>"><br>
+		<?php echo form_error('datefin');?>
 		<input type="submit" value="SEND">
 </form>
