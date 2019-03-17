5bc1d5c8745f0b65d4ed570c9bd4813ce3d02c3f
elviskankola1 <elviskankola1@gmail.com>
2019-03-17 07:14:58 +0200

    final

--- application/views/detail_sup_task.php
+++ application/views/detail_sup_task.php
@@ -3,4 +3,5 @@
         <?php echo $value->description; ?>
     </h1>
     </p>voulez-vous sup cette tache?</p>
-<?php endforeach;?>
+    <a href="<?php echo site_url(); ?>">Annuler</a> <a href="<?php echo site_url('welcome/deleteOneTask/').$value->id; ?>">Supprimer</a>
+<?php endforeach; ?>
