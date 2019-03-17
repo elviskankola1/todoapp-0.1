9b58af520023988c581fa828eaa9b9cd986e250e
elviskankola1 <elviskankola1@gmail.com>
2019-03-17 07:35:52 +0200

    [NEW] utilisation de l'helper  form

--- application/views/detail_task.php
+++ application/views/detail_task.php
@@ -3,8 +3,11 @@
 <h2>
     <form action="<?php echo site_url('welcome/updateOneTask'); ?>" method="POST">
         <input type="text" value="<?php echo $value->description; ?>" name="description"><br>
-        <input type="date" value="<?php echo $value->date_debut; ?>" name="debut"><br>
+        <b style="color:red"><?php echo form_error('description'); ?></b>
+        <input type="date" value="<?php echo $value->date_debut; ?>" name="debut" ><br>
+        <b style="color:red"><?php echo form_error('debut'); ?></b>
         <input type="date" value="<?php echo $value->date_fin; ?>" name="fin"><br>
+        <b style="color:red"><?php echo form_error('fin'); ?></b>
         <input type="hidden" value="<?php echo $value->id; ?>" name="id"><br>
         <input type="submit" value="UPDATE">
     </form>
